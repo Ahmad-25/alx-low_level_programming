a9688f2b0c8b10c0038c32fa74b57a2d49f4e57a
Ahmad-25 <abdulhakeemahmad25@gmail.com>
2023-06-19 00:44:17 +0000

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ *  * call the entry function main-to print a line
+ *   * use puts functon
+ *    * return value should be 0(success)
+ *     */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
