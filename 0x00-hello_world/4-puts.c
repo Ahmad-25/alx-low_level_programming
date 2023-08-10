3e91a603f6a0a2cd1bd582bd6a8802020a6d6503
Ahmad-25 <abdulhakeemahmad25@gmail.com>
2023-08-10 20:55:13 +0000

    Hello world C project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * call the entry function main-to print a line
+ * use puts functon
+ * return value should be 0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
