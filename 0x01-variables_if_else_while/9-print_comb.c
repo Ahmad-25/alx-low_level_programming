74245c5d1be4e3280b34273649093cfbbdd43fc8
Ahmad-25 <abdulhakeemahmad25@gmail.com>
2023-08-14 20:40:50 +0000

    if/else project

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main -entry point
- * Return: always 0 (success)
+ *main -entry point
+ *Return: always 0 (success)
  */
 int main(void)
 {
@@ -10,6 +10,10 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar('0' + num);
+		if (num == 9)
+		{
+			break;
+		}
 		putchar(',');
 		putchar(' ');
 	}
