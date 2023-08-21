09a1dad139aa6a39b6e75c2f5d6ce25ab37b85cb
Ahmad-25 <abdulhakeemahmad25@gmail.com>
2023-08-21 22:51:19 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int y = 0;
+
+	while (*s != '\0')
+	{
+		y++;
+		s++;
+	}
+	return (y)
+}#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int y = 0;
+
+	while (*s != '\0')
+	{
+		y++;
+		s++;
+	}
+	return (y)
+}#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int y = 0;
+
+	while (*s != '\0')
+	{
+		y++;
+		s++;
+	}
+	return (y)
+}
