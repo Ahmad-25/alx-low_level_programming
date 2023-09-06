24459e11a255a7dc50928255fc807d485e63355d
Ahmad-25 <abdulhakeemahmad25@gmail.com>
2023-09-06 23:08:51 +0000

    static library

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @d: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *d, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (d[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		d[i] = src[j];
+		i++;
+		j++;
+	}
+
+	d[i] = '\0';
+	return (d);
+}
+
